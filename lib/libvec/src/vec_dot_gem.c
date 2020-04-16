24eb9bd787e8bee0f2351d875098b399dba5696f
nanaosakisan <nanaosakisan@hotmail.fr>
2020-04-16 15:17:00 +0200

    add normalize functions

--- lib/libvec/src/vec_dot_gem.c
+++ lib/libvec/src/vec_dot_gem.c
@@ -6,7 +6,7 @@
 /*   By: iporsenn <iporsenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 16:13:32 by iporsenn          #+#    #+#             */
-/*   Updated: 2020/03/07 16:21:46 by iporsenn         ###   ########.fr       */
+/*   Updated: 2020/04/16 15:16:27 by iporsenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 inline float   vec4_dot_gem(t_vec4 vec1, t_vec4 vec2, float angle)
 {
-    return (vec4_normalize(vec1) * vec4_normalize(vec2) * cos(angle));
+    return (vec4_norm(vec1) * vec4_norm(vec2) * cos(angle));
 }
 
 inline float   vec3_dot_gem(t_vec3 vec1, t_vec3 vec2, float angle)
 {
-    return (vec3_normalize(vec1) * vec3_normalize(vec2) * cos(angle));
+    return (vec3_norm(vec1) * vec3_norm(vec2) * cos(angle));
 }
 
 inline float   vec2_dot_gem(t_vec2 vec1, t_vec2 vec2, float angle)
 {
-    return (vec2_normalize(vec1) * vec2_normalize(vec2) * cos(angle));
+    return (vec2_norm(vec1) * vec2_norm(vec2) * cos(angle));
 }
